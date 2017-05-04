8d5659b8f3d521f3b1162948aa986ceb659f1c30
wangyunwon <wangyunwon@didichuxing.com>
2017-05-04 21:28:35 +0800

    添加react-addons-perf性能检测工具

--- src/initComponent/TodoItem.js
+++ src/initComponent/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class TodoItem extends React.PureComponent{
   componentDidUpdate(){
-    console.log('did update')
+    // console.log('did update')
   }
   render(){
     return (
